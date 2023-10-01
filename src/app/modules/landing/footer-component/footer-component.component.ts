4a7b0353815842c26594dad6dd7bb875040c07f3
Eduardo Caversan da Silva Rocha <104170972+EduardoCaversan@users.noreply.github.com>
2023-10-01 14:00:13 -0300

    Main update (#1)
    * Bootstrap added and minor changes
    
    * main-update
    
    ---------
    
    Co-authored-by: Eduardo Caversan <eduardo@adven.tech>

--- src/app/modules/landing/footer-component/footer-component.component.ts
+++ src/app/modules/landing/footer-component/footer-component.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer-component.component.scss']
 })
 export class FooterComponentComponent {
+  public year: number = 0;
 
+  ngOnInit() {
+    this.getYear();
+  }
+  
+  public getYear(): void {
+    const date = new Date();
+    this.year = date.getFullYear()
+  }
 }
